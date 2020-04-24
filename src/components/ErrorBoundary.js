753474c904e9d49f79e03cadb03f727ec0d5650f
aqdas002 <42019890+aqdas002@users.noreply.github.com>
2020-04-25 02:49:18 +0530

    added error boundary on cardlist

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,32 @@
+import React ,{Component} from 'react';
+
+
+
+class ErrorBoundary extends Component{
+	constructor(){
+		super();
+		this.state={
+			hasError:false
+		}
+
+	}
+
+
+
+	componentDidCatch(error,info){
+		this.setState({hasError:true
+		})
+
+	}
+
+
+	render(){
+		if (this.state.hasError){
+			return <h1> OOOOOPPSSS... Something is not working </h1>
+
+		}
+		return this.props.children;
+	}
+}
+
+export default ErrorBoundary
