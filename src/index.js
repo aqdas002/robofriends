37aeb63a9955c56789cd89eac06f907f4eff2751
aqdas002 <42019890+aqdas002@users.noreply.github.com>
2020-06-16 22:00:12 +0530

    added redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+import { createStore , applyMiddleware , combineReducers} from 'redux';
+import { searchRobots ,requestRobots } from './reducers';
+import { Provider }  from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({searchRobots,requestRobots})
+const logger =createLogger();
+const store =
+         createStore(rootReducer,
+          applyMiddleware( thunkMiddleware, logger)
+          );
+
+ReactDOM.render(
+	             <Provider store ={store}>
+                    <App />
+                 </Provider>, document.getElementById('root'));
 registerServiceWorker();
